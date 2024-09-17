5af110e83a8feea06306bd814d92f17487aa0ff1
vasavi-durai <vasavinexusdigitalia@gmail.com>
2024-09-17 15:57:53 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:beauty_app/splash_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+        body: SplashScreen(),),
+  ),
+  );
+
+}
